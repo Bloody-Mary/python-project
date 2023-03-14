79dae28ae605ba9a6300cae56f86561e82e46463
Bloody-Mary <marianelalieben@gmail.com>
2023-03-14 16:07:51 +0300

    edited brain_calc4

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -7,7 +7,7 @@ NUM2 = randint(1, 50)
 OPERATIONS = ('+', '-', '*')
 
 
-def operation_choice(operation, correct_answer, NUM1, NUM2):
+def operation_choice(operation, NUM1, NUM2):
     if operation == '+':
         correct_answer = NUM1 + NUM2
     elif operation == '-':
@@ -20,4 +20,5 @@ def operation_choice(operation, correct_answer, NUM1, NUM2):
 def get_question_and_answer():
     operation = choice(OPERATIONS)
     question = f"{NUM1} {operation} {NUM2}"
+    correct_answer = str(calculate(operation, NUM1, NUM2))
     return correct_answer, question
