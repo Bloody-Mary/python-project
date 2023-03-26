ca850b41293c470b2c812132a67ec31b89aaf46d
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-03-26 20:18:24 +0300

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,16 +5,15 @@ from brain_games.scripts import utils
 GAME_RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 FIRST_NUM = 1
 LAST_NUM = 100
+SOME_VAR = True
 
 
 def is_even(number):
     if number % 2 == 0:
-        return correct_answer == True
+        return utils.boolean_to_answer(SOME_VAR)
 
 
 def get_question_and_answer():
     question = randint(FIRST_NUM, LAST_NUM)
-    correct_answer = True
-    if is_even(question):
-        return utils.boolean_to_answer(correct_answer)
-    return str(correct_answer), question
+    correct_answer = is_even(question)
+    return correct_answer, question
