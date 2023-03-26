fdee88fd2d63454869ac90631fc343778adb24e2
Bloody-Mary <marianelalieben@gmail.com>
2023-03-26 14:26:31 +0300

    Update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -10,8 +10,8 @@ LAST_NUM = 100
 def get_question_and_answer():
     num = randint(FIRST_NUM, LAST_NUM)
     question = str(num)
-    rand_num = num % 2
-    if utils.boolean_to_answer(rand_num) == 0:
+    rand_num = (num % 2 == 0)
+    if utils.boolean_to_answer(rand_num) == True:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
