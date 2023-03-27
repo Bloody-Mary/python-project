7bec55e7ba4a23a8ed8f2623496999ae244c27b5
Bloody-Mary <37978402+Bloody-Mary@users.noreply.github.com>
2023-03-27 13:21:33 +0300

    Update brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -21,8 +21,4 @@ def is_prime(num):
 
 def get_question_and_answer():
     question = randint(FIRST_NUM, LAST_NUM)
-    if utils.boolean_to_answer(question):
-       correct_answer = 'yes'
-    else:
-       correct_answer = 'no'
-    return correct_answer, question
+    return utils.boolean_to_answer(is_prime(question)), question
